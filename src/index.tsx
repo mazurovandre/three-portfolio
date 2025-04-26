419f9c17b8e88eead81f42ad2d72f351bb9865cf
Andrey Mazurov <mazurovandre@gmail.com>
2025-04-26 15:15:26 +0300

    Moved to vite

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import App from './App';
 import { ThemeProvider } from 'styled-components';
 import { lightTheme } from './theme';
 import GlobalStyles from './styles/GlobalStyles';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -19,8 +18,3 @@ root.render(
     </ThemeProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
